1e1e70877b941901ffcc6f81088d096ae4b3d444
muhammadsyaarif <148513751+muhammadsyaarif@users.noreply.github.com>
2024-07-02 10:09:44 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,10 +27,11 @@ export default function Home() {
   }, []);
 
   const getLampuColor = (ketinggian: any) => {
-    if (ketinggian <= 10) {
+    const adjustedKetinggian = 20 - ketinggian; // Adjust the ketinggian
+    if (adjustedKetinggian <= 10) {
       beepSound.play();
       return 'red'; 
-    } else if (ketinggian <= 15) {
+    } else if (adjustedKetinggian <= 15) {
       beepSound.stop();
       return 'yellow'; 
     } else {
@@ -40,10 +41,11 @@ export default function Home() {
   };
 
   const getLampuKeterangan = (ketinggian: any) => {
-    if (ketinggian <= 10) {
+    const adjustedKetinggian = 20 - ketinggian; // Adjust the ketinggian
+    if (adjustedKetinggian <= 10) {
       beepSound.play();
       return 'Awas';
-    } else if (ketinggian <= 15) {
+    } else if (adjustedKetinggian <= 15) {
       beepSound.stop();
       return 'Waspada';
     } else {
@@ -59,64 +61,66 @@ export default function Home() {
   return (
     <>
       <div>
-        {data.map((item, index) => (
-          <div key={index}>
-            <div className="flex flex-col mx-10 my-10">
-              <div className="flex flex-col justify-center items-center p-6">
-                <p className="text-4xl font-semibold uppercase tracking-widest text-white">Smart Dam</p>
-              </div>
-              <div className="lg:grid lg:grid-cols-4 gap-4 m-4">
-                <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
-                  <p className="mb-2">Ketinggian Air</p>
-                  <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                  <p className="text-4xl">{item.jarak} <span className="text-base">cm</span></p>
+        {data.map((item, index) => {
+          const adjustedJarak = 20 - item.jarak; // Adjust the jarak
+          return (
+            <div key={index}>
+              <div className="flex flex-col mx-10 my-10">
+                <div className="flex flex-col justify-center items-center p-6">
+                  <p className="text-4xl font-semibold uppercase tracking-widest text-white">Smart Dam</p>
                 </div>
-                <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
-                  <p className="mb-2">Indikator Bahaya</p>
-                  <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div className="flex items-center">
-                      <p className="font-bold">{getLampuKeterangan(item.jarak)}</p>
+                <div className="lg:grid lg:grid-cols-4 gap-4 m-4">
+                  <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
+                    <p className="mb-2">Ketinggian Air</p>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                    <p className="text-4xl">{adjustedJarak} <span className="text-base">cm</span></p>
+                  </div>
+                  <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
+                    <p className="mb-2">Indikator Bahaya</p>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div className="flex items-center">
+                        <p className="font-bold">{getLampuKeterangan(item.jarak)}</p>
+                      </div>
+                      <div className="flex items-center justify-end">
+                        <div
+                          className={`lampu ${getLampuColor(item.jarak)}`}
+                        >
+                        </div>
+                      </div>
                     </div>
-                    <div className="flex items-center justify-end">
-                      <div
-                        className={`lampu ${getLampuColor(item.jarak)}`}
-                      >
+                  </div>
+                  <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
+                    <p className="mb-2">Indikator Hujan</p>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div>
+                        <p className="text-4xl">{item.hujan}</p>
+                      </div>
+                      <div>
+                        <p>{getRainKeterangan(item.hujan)}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
+                    <p className="mb-2">Suhu Sekitar</p>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div>
+                        <p>Suhu</p>
+                        <p>{item.suhu}</p>
+                      </div>
+                      <div>
+                        <p>Kelembaban</p>
+                        <p>{item.kelembaban}</p>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
-                  <p className="mb-2">Indikator Hujan</p>
-                  <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div>
-                      <p className="text-4xl">{item.hujan}</p>
-                    </div>
-                    <div>
-                      <p>{getRainKeterangan(item.hujan)}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-4">
-                  <p className="mb-2">Suhu Sekitar</p>
-                  <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                  <div className="grid grid-cols-2 gap-4">
-                    <div>
-                      <p>Suhu</p>
-                      <p>{item.suhu}</p>
-                    </div>
-                    <div>
-                      <p>Kelembaban</p>
-                      <p>{item.kelembaban}</p>
-                    </div>
-                  </div>
-                </div>
-
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </>
   );
